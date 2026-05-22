53885c5e706366bb7dcaf8bb64dd78b815e15ec9
baidar-10 <baidar.zh@gmail.com>
2026-05-22 15:34:52 +0500

    Date from, date to translated

--- src/pages/bookings/ManageBookingsPage.tsx
+++ src/pages/bookings/ManageBookingsPage.tsx
@@ -488,7 +488,7 @@ export default function ManageBookingsPage() {
           </select>
         </label>
         <label className="text-sm text-secondary">
-          Date from
+          Дата с
           <input
             type="datetime-local"
             value={dateFrom}
@@ -500,7 +500,7 @@ export default function ManageBookingsPage() {
           />
         </label>
         <label className="text-sm text-secondary">
-          Date to
+          Дата по
           <input
             type="datetime-local"
             value={dateTo}
